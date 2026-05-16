24099ba74d02f853de83460c329538f5ca97d8a8
gustavoscruz29 <gustavoscruz29@gmail.com>
2026-05-16 15:50:23 -0300

    Create moveBrackets.cpp

---
+++ 1000/moveBrackets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define REP(i,a,b) for(int i = a; i < b; i++)
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(0);
+typedef long long ll;
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+  int t; cin >> t;
+  while(t--){
+    int n; cin >> n;
+    string s; cin >> s;
+    int sum = 0, operations = 0;
+    REP(i,0,n){
+      if(s[i] == '(') sum++;
+      else sum--;
+      if(sum < 0){
+        sum = 0; operations++;
+      }
+    }
+    cout << operations << "\n";
+  }
+  
+  return 0;
+}

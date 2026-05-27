386f97cbdc2a824d4d61764218778277a2009646
gustavoscruz29 <gustavoscruz29@gmail.com>
2026-05-27 11:23:26 -0300

    Create againTwentyFive.cpp

---
+++ codeforces/800/againTwentyFive.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    
+    long long n;
+    cin >> n;
+    cout << 25;
+    
+    return 0;
+}

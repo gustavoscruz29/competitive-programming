7141ed01a64c9ca7ab1b166255aafffdd207f17a
gustavoscruz29 <gustavoscruz29@gmail.com>
2026-06-12 16:46:35 -0300

    Create complicatedGCD.cpp

---
+++ codeforces/800/complicatedGCD.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(0);
+#define REP(i,a,b) for(int i = a; i < b; i++)
+#define endl "\n"
+typedef long long ll;
+ 
+using namespace std;
+ 
+int main(){
+  string a, b;
+  cin >> a >> b;
+  if(a == b){
+    cout << a << endl;
+  }
+  else{
+    cout << 1 << endl;
+  }
+  return 0;
+}
